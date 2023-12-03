d4eba565de9f8cf861dd996a0f376ab9bfd16eca
andrepinto <andrep@sepiocyber.com>
2023-12-03 21:30:58 +0000

    Adding solutions for day03

---
+++ day03/python/part2.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+
+
+def exists_symbol_nearby(start_pos: tuple[int, int],
+                         len_number: int,
+                         map: dict[tuple[int, int], str],):
+    for i in range(max(start_pos[0] - 1, 0), start_pos[0] + 2):
+        for j in range(max(start_pos[1] - 1, 0), start_pos[1] + len_number + 1):
+            if result := map.get((i, j)):
+                if result == '*':
+                    return (i, j)
+    return False
+
+
+def main(file_path: str):
+    cand_gears = defaultdict(list)
+    with open(file_path) as file:
+        map = {}
+        for i, line in enumerate(file):
+            for j, char in enumerate(line.strip() + '.'):
+                map[i, j] = char
+        current_number = ''
+        for key, value in map.items():
+            if str.isnumeric(value):
+                if current_number == '':
+                    start_pos = key
+                current_number += value
+            else:
+                if current_number != '':
+                    final_j = key[1]
+                    number_lenght = final_j - start_pos[1]
+                    if (symbol_pos := exists_symbol_nearby(start_pos, number_lenght, map)):
+                        cand_gears[symbol_pos].append(current_number)
+                    current_number = ''
+
+    gears = [value for _, value in cand_gears.items() if len(value) == 2]
+    sum = 0
+    for gear in gears:
+        code = 1
+        for part in gear:
+            code *= int(part)
+        sum += code
+    return sum
+
+
+if __name__ == '__main__':
+    print(main('files/input.txt'))
---
+++ day03/python/test1.py
@@ -0,0 +1,5 @@
+from . import part1
+
+
+def test_main():
+    assert part1.main('files/test_input.txt') == 4361
